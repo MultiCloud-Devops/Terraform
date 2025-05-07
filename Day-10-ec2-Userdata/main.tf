c23e04b92ca41c404f73a8425dcc62b0a8bacc9e
MultiCloud-Devops <krishnaurs2022@gmail.com>
2025-05-07 14:20:12 +0530

    tested-jenkins-trigger-github-hook

--- Day-10-ec2-Userdata/main.tf
+++ Day-10-ec2-Userdata/main.tf
@@ -2,4 +2,7 @@ resource "aws_instance" "dev_userdata_server" {
    ami="ami-0f1dcc636b69a6438"
    instance_type = "t2.micro"
    user_data = file("httpd.sh")
+   tags = {
+      Name="jenkins_github_hook_trigger_server"
+   }
 }
